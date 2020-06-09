1d968e6928b5669edea788be8019e74015c3e399
corporatesnub <ddimela9no@gmail.com>
2020-06-08 22:16:33 -0400

    Edited app.js content to display some love

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Te amo gabita, no te presiones tanto.
         </p>
         <a
           className="App-link"
